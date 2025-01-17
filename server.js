7a82b3b03882d6edd8c970e3ac144f16518f4078
flaman <irakozeflamanc@gmail.com>
2025-01-18 01:09:10 +0200

    chore: allow origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS configuration
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://track-finance-app.netlify.app/",
     credentials: true,
   })
 );
